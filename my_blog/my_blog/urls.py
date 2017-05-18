26c28bf00c3543120c3922b9cca9701214d9bb7e
seasons <rong170505@163.com>
2017-05-18 14:48:28 +0800

    django tutorial init

---
+++ my_blog/my_blog/urls.py
@@ -0,0 +1,23 @@
+﻿"""my_blog URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.8/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import include, url
+from django.contrib import admin
+
+urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'article.views.home'),  #由于目前只有一个app, 方便起见, 就不设置include了
+#    url(r'^\d+/$', 'article.views.detail', name='detail'),
+#    url(r'^test/$', 'article.views.test'),
+]
